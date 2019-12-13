4f4db51d2a01f76aa0db0770997606ef62c50bed
Richard Naab <naabrich@desy.de>
2019-12-13 14:06:59 +0100

    fix insertions event1,event2

--- ofu_old_comments.py
+++ ofu_old_comments.py
@@ -61,19 +61,36 @@ class multiplets():
         time_diff = sub_diff(self.data['time'],n_lag)
 
         while np.any( time_diff<self._delta_t ):
+            #print('n_lag: ',n_lag)
+
             space_diff = np.abs( ofu_ang_distance(self.data[:-n_lag] , self.data[n_lag:]) )
             total_cut = np.logical_and(space_diff<self._delta_ang,time_diff<self._delta_t)
+
+            #print(np.where(space_diff<self._delta_ang))
+            #print(np.where(time_diff<self._delta_t))
+            #print('n_lag: ',n_lag)
+            #print('Found any doublet pair?',np.any(total_cut))
             if np.any(total_cut):
                 # defines the two events for each doublet:
                 if first_events == True:
+                    #print('create events')
                     event1 = np.copy(self.data[:-n_lag][total_cut])
                     event2 = np.copy(self.data[n_lag:][total_cut])
                     first_events = False
+                    #print('Events: ',event1,event2)
                 else: # insert at correct times
+                    #print('insert events')
+                    #print('Before: # events: ',len(event1))
+                    #print(self.data[:-n_lag][total_cut])
+                    #print(event1['time'],self.data['time'][:-n_lag][total_cut])
                     e1_insert = np.searchsorted(event1['time'],self.data[:-n_lag][total_cut]['time'])
                     e2_insert = np.searchsorted(event2['time'],self.data[n_lag:][total_cut]['time'])
+                    #print('Insertions: ',e1_insert,e2_insert)
+
                     event1 = np.insert(event1,e1_insert,self.data[:-n_lag][total_cut])
                     event2 = np.insert(event2,e2_insert,self.data[n_lag:][total_cut])
+                    #print('Events: ',event1,event2)
+                    #print('After: # events: ',len(event1))
             n_lag += 1
             time_diff = sub_diff(self.data['time'],n_lag)
 
@@ -82,10 +99,12 @@ class multiplets():
             doublets=ofu_doublet_average(event1, event2 ,self._theta_a)
             self._n_multiplets = 2
             self._multiplets.append(doublets)
+            #print('Doublets scanned.')
 
             # scan here for higher multiplicities:
             uni,index,inv,counts = np.unique(event2,return_index=True, return_inverse=True, return_counts=True)
             if np.any(counts>=2):
+                #print('Test for higher multiplicities:')
                 candidates, = np.where(counts>=2)
                 for cand in candidates:
                     mask = inv==cand    # event1[mask] are all events that form a doublet with cand
@@ -99,9 +118,71 @@ class multiplets():
             print('No doublets found')
 
 
+        # # can also have doublets that are not subsequent events:
+        # n_lag = 2
+        # additional_time_diff = np.abs(np.diff(self.data['time'], n=n_lag ))
+        # while np.sum( additional_time_diff<self._delta_t ):
+        #     additional_space_diff = ofu_ang_distance(self.data[:-n_lag] , self.data[n_lag:])
+        #     additional_cut = np.logical_and( additional_time_diff<self._delta_t , additional_space_diff<self._delta_ang )
+        #     if np.sum(additional_cut)>0:
+        #         #print('Found lagged doublets with n_lag = ',n_lag)
+        #         # insert return self._multiplets[0]events sorted in time:
+        #         e1_insert = np.searchsorted(event1['time'],self.data[:-n_lag][additional_cut]['time'])
+        #         e2_insert = np.searchsorted(event2['time'],self.data[n_lag:][additional_cut]['time'])
+        #         np.insert(event1,e1_insert,self.data[:-n_lag][additional_cut])
+        #         np.insert(event1,e2_insert,self.data[n_lag:][additional_cut])
+        #     n_lag += 1
+        #     additional_time_diff = np.abs(np.diff(self.data['time'], n=n_lag ))
+
+
+            # last_ntet = False
+            # multiplicity = 2
+            # while not last_ntet:
+            #     # find current multiplicity in data?
+            #     #time_diff = np.abs(np.diff(self.data['time'], n=multiplicity-1 ) )
+            #
+            #     if multiplicity==2: #case of the doublets
+
+            # else:
+            #     if self._only_doublets:
+            #         last_ntet = True
+            #         print('Only Doublets processed')
+            #     else:
+            #         #time_diff = np.abs(np.diff(self.data['time'], n=multiplicity-1 ) )
+            #         time_diff = sub_diff(self.data['time'],multiplicity-1)
+            #         # test all sub-pairs, where at least x pairs have to fulfill the spacial distance criterion
+            #         m = multiplicity-1
+            #         space_diff = np.abs( ofu_ang_distance(self.data[:-m] , self.data[m:]) )
+            #         space_cut = space_diff<self._delta_ang
+            #         for i in range(m,0,-1):
+            #             for j in range(i):
+            #                 #print('Testing indices: ',m-i,-i,' ',m-j,-j)
+            #                 if j!=0:
+            #                     space_diff_2 = ofu_ang_distance(self.data[m-i:-i] , self.data[m-j:-j])
+            #                 else: # slice [a:-0] would give [] ..
+            #                     space_diff_2 = ofu_ang_distance(self.data[m-i:-i] ,self.data[m-j:])
+            #                 space_cut = np.logical_and(space_cut,np.abs(space_diff_2<self._delta_ang) )
+            #
+            #         if np.sum( np.logical_and( np.abs(time_diff<self._delta_t) , space_cut ) )>0:
+            #         # at least one pair fulfills criterion
+            #             self._n_multiplets = multiplicity
+            #
+            #         # hiher multiplicity doublets have no TS defined, so far nothing done to them
+            #             print('Found a (at least one) multiplet with multiplicity {} in the dataset!'.format(multiplicity))
+            #             multiplicity+=1
+            #
+            #         else:
+            #             last_ntet = True
+            #             print('Multiplets processed')
+
+
     def info(self):
         print(self.data.dtype)
         print(np.min(self.data['angErr']))
 
+
+
+
+    #def get_doublets():
 
     #def get_multiplets():
