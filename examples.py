869eac524f9b7eb2e10b412201ea600d15332b08
Richard Naab <naabrich@desy.de>
2019-12-17 18:09:18 +0100

    reorganized examples

---
+++ examples.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# The datatype used in the IceCube sets is:
+#datatype = [('run', '<u4'), ('event', '<u4'), ('subevent', '<u4'),
+#       ('time', '<f8'), ('azi', '<f8'), ('zen', '<f8'), ('ra', '<f8'), ('dec', '<f8'), ('angErr', '<f8'), ('logE', '<f8')]
+
+
+
+#set = np.load('path_to_npy_dataset..')
+#test = ofu.multiplets(set,100,3.5,floor=None)
+#doublets = test.get_doublets
